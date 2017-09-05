c0886a9414cdc4064c4d46d3b8f3a4afaa16a773
Nirvan <nirry_gelda@hotmail.com>
2017-09-05 23:16:07 +1000

    delete objects after a certain height

---
+++ papertoss/Assets/Scripts/RemoveObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveObject : MonoBehaviour {
+
+    public GameObject gameObject; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.y < -200)
+        {
+            Destroy(gameObject); 
+        }
+	}
+}
